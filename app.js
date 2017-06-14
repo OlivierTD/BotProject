607c06823a4c50427efe1a14ef4281c82b06ba9d
OlivierTD <oli.trep@gmail.com>
2017-06-14 09:10:03 -0400

    Added basic bot functionnality.

---
+++ app.js
@@ -0,0 +1,25 @@
+var restify = required("restify");
+var builder = required("botbuilder");
+
+//Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3000, function () {
+    console.log("%s listening to %s", server.name, server.url);
+});
+
+//Chat connector
+var connector = new builder.ChatConnector({
+    appId: process.env.APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+//Listen for messages
+server.post('api/messages', connector.listen());
+
+//Receive message from user and respond accordingly.
+var bot = new builder.UniversalBot(connector, function(session) {
+    session.send("You said: %s", session.message.text);
+});
+
+
+
