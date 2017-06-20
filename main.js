6606337475f97752987f1e12be4dd277b8abde35
OlivierTD <oli.trep@gmail.com>
2017-06-20 17:33:26 +0000

    fix typo in fn name

---
+++ main.js
@@ -0,0 +1,30 @@
+var restify = require("restify");
+var builder = require("botbuilder");
+
+//Restify Server
+var server = restify.createServer();
+
+//Chat connector
+var connector = new builder.ChatConnector({
+    appId: process.env.APP_ID,
+    appPassword: process.env.APP_PASSWORD
+});
+
+//Listen for messages
+server.post('api/messages', connector.listen());
+
+//Receive message from user and respond accordingly.
+var bot = new builder.UniversalBot(connector, function(session) {
+    session.send("You said: %s", session.message.text);
+});
+
+server.get('/', restify.serveStatic ({
+    directory: __dirname,
+    default: '/index.html'
+}));
+
+server.listen(process.env.PORT || 8081);
+
+
+console.log("Hello World from console.")
+
