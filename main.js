6caff3bef34108dcfa72cbda0ef6686b77029e9d
OlivierTD <oli.trep@gmail.com>
2017-06-22 19:00:37 +0000

    Changing console logs.

--- main.js
+++ main.js
@@ -16,8 +16,7 @@ server.post('api/messages', connector.listen());
 
 //Receive message from user and respond accordingly.
 var bot = new builder.UniversalBot(connector, function(session) {
-    session.send(session + "test");
-//    session.send("You said: %s", session.message.text);
+    session.send("You said: %s", session);
 });
 
 server.get('/', restify.serveStatic ({
