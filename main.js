1b9518bf793c549f18f43659401502b38cc9a2bc
OlivierTD <oli.trep@gmail.com>
2017-06-22 19:09:19 +0000

    Forgot a ,

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ server.post('api/messages', connector.listen());
 //Receive message from user and respond accordingly.
 var bot = new builder.UniversalBot(connector, function(session) {
     session.send("You said: %s", session.message.text);
-    session.send("Session info: " session);
+    session.send("Session info: ", session);
 });
 
 server.get('/', restify.serveStatic ({
