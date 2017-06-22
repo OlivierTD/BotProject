c8e2554729c814f74289d2a2eaa468f4b5598b9e
OlivierTD <oli.trep@gmail.com>
2017-06-22 16:28:12 +0000

    Fixed console logs.

--- main.js
+++ main.js
@@ -10,20 +10,22 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.APP_PASSWORD
 });
 
+
 //Listen for messages
-//server.post('api/messages', connector.listen());
+server.post('api/messages', connector.listen());
 
 //Receive message from user and respond accordingly.
-//var bot = new builder.UniversalBot(connector, function(session) {
-//    session.send("You said: %s", session.message.text);
-//});
+var bot = new builder.UniversalBot(connector, function(session) {
+    session.send("You said: %s", session.message.text);
+});
 
 server.get('/', restify.serveStatic ({
     directory: __dirname,
     default: '/index.html'
 }));
 
-console.log('BOOTING !, binding port follows')
-console.log('HERE IS THE ENV VARIABLE:',process.env.PORT, process.env.port)
+console.log("CHAT CONNECTOR: ", process.env.APP_ID, process.env.APP_PASSWORD);
+console.log('BOOTING !, binding port follows');
+console.log('HERE IS THE ENV VARIABLE:',process.env.PORT, process.env.port);
 console.log(process.env.PORT || 8081);
 server.listen(process.env.PORT || 8081);
