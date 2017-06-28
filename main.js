08e7c2b6c35a896e16dc351f8b800fa6652fd40e
OlivierTD <oli.trep@gmail.com>
2017-06-28 14:22:16 +0000

    Fixed typo

--- main.js
+++ main.js
@@ -136,8 +136,8 @@ bot.dialog('address', function (session, args) {
                 }); 
             }
             else {
-                console.log("Type of Data: " + (typeof data);
-                session.send("Data: " + (typeof data);
+                console.log("Type of Data: " + (typeof data));
+                session.send("Data: " + (typeof data));
                 session.send("You are already registered in dynamoDB!");
                 session.endDialog();
             }
