060f6aa22ed4e1fa244c03190c80b0364b0b8da8
OlivierTD <oli.trep@gmail.com>
2017-07-04 17:56:53 +0000

    refactored the code.

--- main.js
+++ main.js
@@ -222,53 +222,6 @@ bot.dialog('approbation', [
         }
 ]);
 
-//    var card = new builder.HeroCard(session)
-//        .title('Approval Request')
-//        .subtitle('trx-id: 9999-9999-9990')
-//        .text('Amount: 0.000000000000001$')
-//        .buttons([
-//                builder.CardAction.dialogAction(session, 'approbation-answer', 'approved', 'Approve'),
-//                builder.CardAction.dialogAction(session, 'approbation-answer', 'declined', 'Decline')
-//        ]);
-//    var msg = new builder.Message(session).addAttachment(card);
-//
-//
-//
-//    session.send(msg);
-//
-//    session.endDialog(); 
-//}); 
-
-//bot.beginDialogAction('approbation-answer', 'approbationAnswer');
-//
-//bot.dialog('approbationAnswer', function(session, args){
-//    
-//    var params = {
-//        Item: {
-//            requestID: "5555-5555-5556",
-//            accountID: "0000-0000-0001",
-//            trxCode: "9999-9999-9990",
-//            state: "approved"
-//        },
-//        TableName: "requests"
-//    };
-//
-//    docClient.put(params, function(err, data) {
-//        if (err) {
-//            console.log(err, err.stack);
-//            session.send("Error occured when trying to put in dynamoDB: ", err);
-//            session.endDialog();
-//        }
-//        else {
-//            console.log("Successfully registered data in dynamoDB: " + JSON.stringify(params));
-//            session.send("Successfully registered data in dynamoDB: " + JSON.stringify(params));
-//            session.endDialog();
-//        }
-//    });
-//    
-//    session.endDialog("You %s the transaction", args.data);
-//});
-
 function approval(session, value) {
     var params = {
         Item: {
