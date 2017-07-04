1f5d335d234f43d0d2026822bbd419f169ee2be0
OlivierTD <oli.trep@gmail.com>
2017-07-04 15:17:31 +0000

    trying to send msg following slack doc.

--- main.js
+++ main.js
@@ -94,32 +94,32 @@ server.get('/approbation', function(req, res, next) {
 
 
 //Where server receives answer from bot for approbation
-//server.post('/approb-answer', function approval(req, res, next) {
-//    
-//    var params = {
-//        Item: {
-//            requestID: "5555-5555-5556",
-//            accountID: "0000-0000-0001",
-//            trxCode: "9999-9999-9990",
-//            state: "approved"
-//        },
-//        TableName: "requests"
-//    };
-//
-//    docClient.put(params, function(err, data) {
-//        if (err) {
-//            console.log(err, err.stack);
-//            session.send("Error occured when trying to put in dynamoDB: ", err);
-//            session.endDialog();
-//        }
-//        else {
-//            console.log("Successfully registered data in dynamoDB: " + JSON.stringify(params));
-//            session.send("Successfully registered data in dynamoDB: " + JSON.stringify(params));
-//            session.endDialog();
-//        }
-//    }); 
-//    
-//});
+server.post('/approb-answer', function approval(req, res, next) {
+    
+    var params = {
+        Item: {
+            requestID: "5555-5555-5556",
+            accountID: "0000-0000-0001",
+            trxCode: "9999-9999-9990",
+            state: "approved"
+        },
+        TableName: "requests"
+    };
+
+    docClient.put(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            session.send("Error occured when trying to put in dynamoDB: ", err);
+            session.endDialog();
+        }
+        else {
+            console.log("Successfully registered data in dynamoDB: " + JSON.stringify(params));
+            session.send("Successfully registered data in dynamoDB: " + JSON.stringify(params));
+            session.endDialog();
+        }
+    }); 
+    
+});
 
 
 //Receive message from user and respond accordingly.
@@ -200,54 +200,82 @@ bot.dialog('address', function (session, args) {
 
 });
 
-//Basic dialog to test approbation.
+//Basi dialog to test approbation.
 bot.dialog('approbation', function(session, args) {
     session.send('Hello world from approbation.');
     
-    var card = new builder.HeroCard(session)
-        .title('Approval Request')
-        .subtitle('trx-id: 9999-9999-9990')
-        .text('Amount: 0.000000000000001$')
-        .buttons([
-                builder.CardAction.dialogAction(session, 'approbation-answer', 'approved', 'Approve'),
-                builder.CardAction.dialogAction(session, 'approbation-answer', 'declined', 'Decline')
-        ]);
-    var msg = new builder.Message(session).addAttachment(card);
+    //var card = new builder.HeroCard(session)
+    //    .title('Approval Request')
+    //    .subtitle('trx-id: 9999-9999-9990')
+    //    .text('Amount: 0.000000000000001$')
+    //    .buttons([
+    //            builder.CardAction.dialogAction(session, 'approbation-answer', 'approved', 'Approve'),
+    //            builder.CardAction.dialogAction(session, 'approbation-answer', 'declined', 'Decline')
+    //    ]);
+    //var msg = new builder.Message(session).addAttachment(card);
+
+    var msg = 
+    {
+        "text": "Approval Request",
+        "attachments": [
+            {
+                "text": "trx-id: 9999-9999-9990 \nAmount: 0.00000000001$",
+                "fallback": "Oops, something went wrong.",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "approbation",
+                        "text": "Approve",
+                        "type": "button",
+                        "value": "approve"
+                    },
+                    {
+                        "name": "approbation",
+                        "text": "Decline",
+                        "type": "button",
+                        "value": "decline",
+                    }
+                ]
+            }
+        ]
+    }
+
 
     session.send(msg);
 
     session.endDialog(); 
 }); 
 
-bot.beginDialogAction('approbation-answer', 'approbationAnswer');
-
-bot.dialog('approbationAnswer', function(session, args){
-    
-    var params = {
-        Item: {
-            requestID: "5555-5555-5556",
-            accountID: "0000-0000-0001",
-            trxCode: "9999-9999-9990",
-            state: "approved"
-        },
-        TableName: "requests"
-    };
-
-    docClient.put(params, function(err, data) {
-        if (err) {
-            console.log(err, err.stack);
-            session.send("Error occured when trying to put in dynamoDB: ", err);
-            session.endDialog();
-        }
-        else {
-            console.log("Successfully registered data in dynamoDB: " + JSON.stringify(params));
-            session.send("Successfully registered data in dynamoDB: " + JSON.stringify(params));
-            session.endDialog();
-        }
-    });
-    
-    session.endDialog("You %s the transaction", args.data);
-});
+//bot.beginDialogAction('approbation-answer', 'approbationAnswer');
+//
+//bot.dialog('approbationAnswer', function(session, args){
+//    
+//    var params = {
+//        Item: {
+//            requestID: "5555-5555-5556",
+//            accountID: "0000-0000-0001",
+//            trxCode: "9999-9999-9990",
+//            state: "approved"
+//        },
+//        TableName: "requests"
+//    };
+//
+//    docClient.put(params, function(err, data) {
+//        if (err) {
+//            console.log(err, err.stack);
+//            session.send("Error occured when trying to put in dynamoDB: ", err);
+//            session.endDialog();
+//        }
+//        else {
+//            console.log("Successfully registered data in dynamoDB: " + JSON.stringify(params));
+//            session.send("Successfully registered data in dynamoDB: " + JSON.stringify(params));
+//            session.endDialog();
+//        }
+//    });
+//    
+//    session.endDialog("You %s the transaction", args.data);
+//});
 
 bot.dialog('approb-answer', function(session, args) {
    session.send('Hello world from approb-answer'); 
