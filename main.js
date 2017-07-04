85b9bbcf177762a43f835c2404a0c908d9266bed
OlivierTD <oli.trep@gmail.com>
2017-07-04 15:02:23 +0000

    Testing if previous version was working.

--- main.js
+++ main.js
@@ -94,66 +94,32 @@ server.get('/approbation', function(req, res, next) {
 
 
 //Where server receives answer from bot for approbation
-server.post('/approb-answer', function approval(req, res, next) {
-    
-    var params = {
-        Item: {
-            requestID: "5555-5555-5556",
-            accountID: "0000-0000-0001",
-            trxCode: "9999-9999-9990",
-            state: "approved"
-        },
-        TableName: "requests"
-    };
-
-    docClient.put(params, function(err, data) {
-        if (err) {
-            console.log(err, err.stack);
-            session.send("Error occured when trying to put in dynamoDB: ", err);
-            session.endDialog();
-        }
-        else {
-            console.log("Successfully registered data in dynamoDB: " + JSON.stringify(params));
-            session.send("Successfully registered data in dynamoDB: " + JSON.stringify(params));
-            session.endDialog();
-        }
-    });
-    
-    var userInfo;
-    var params = {
-        TableName: table,
-        Key: {
-            id: 'U62N5L0EL:T63CBFV98' //Hardcoded for now, but could be any id.
-        }
-    };
-    
-    //Attempt to fetch the address information of the user's conversation
-    docClient.get(params, function(err, data){
-        if (err) {
-            console.log("Error while trying to fetch the user for approbation: ", err);
-        }
-        else {
-            console.log("Successfully fetched the user's information for approbation: ");
-            userInfo = data.Item;   
-            var address = { id: userInfo.otherId,
-                channelId: userInfo.channelid,
-                user: {
-                    id: userInfo.id,
-                    name: userInfo.userName },
-                conversation: { id: userInfo.conversationid },
-                bot: {
-                    id: userInfo.botId,
-                    name: userInfo.botName },
-                serviceUrl: userInfo.serviceURL };
-    
-            console.log("Address to send approbation: ", address);
-            res.send('triggered bot approbation successfully. Here\'s the address info: ' + JSON.stringify(address));
-            
-            bot.beginDialog(address, "approb-answer");
-        }
-    });
-    
-});
+//server.post('/approb-answer', function approval(req, res, next) {
+//    
+//    var params = {
+//        Item: {
+//            requestID: "5555-5555-5556",
+//            accountID: "0000-0000-0001",
+//            trxCode: "9999-9999-9990",
+//            state: "approved"
+//        },
+//        TableName: "requests"
+//    };
+//
+//    docClient.put(params, function(err, data) {
+//        if (err) {
+//            console.log(err, err.stack);
+//            session.send("Error occured when trying to put in dynamoDB: ", err);
+//            session.endDialog();
+//        }
+//        else {
+//            console.log("Successfully registered data in dynamoDB: " + JSON.stringify(params));
+//            session.send("Successfully registered data in dynamoDB: " + JSON.stringify(params));
+//            session.endDialog();
+//        }
+//    }); 
+//    
+//});
 
 
 //Receive message from user and respond accordingly.
